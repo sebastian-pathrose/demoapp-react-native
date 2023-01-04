0ce0dea33a1475ebdf259ec84d2261a4fec27bfd
sebastian-pathrose <103589760+sebastian-pathrose@users.noreply.github.com>
2023-01-04 00:38:55 -0800

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Button, Text, TextInput, View, StyleSheet} from 'react-native';
+const App = () => {
+  const [message, setMessage] = React.useState();
+  return (
+    <View>
+      <Button
+        title="Hello to the tester"
+        onPress={() => {
+          setTimeout(() => {
+            setMessage('Hello Tester');
+          }, Math.floor(Math.random() * 200));
+        }}
+      />
+      {message && (
+        <Text style={styles.messageText} testID="printed-message">
+          {message}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  messageText: {
+    fontFamily: 'Arial',
+    fontSize: 38,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+});
+export default App;
